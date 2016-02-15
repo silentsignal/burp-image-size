5b1a4ef80153bf311d834328116eb6e71ce2462e
András Veres-Szentkirályi <vsza@vsza.hu>
2016-02-15 16:05:17 +0100

    replaced homebrew int parsing with ByteBuffer

--- src/burp/SimpleImageSizeReader.java
+++ src/burp/SimpleImageSizeReader.java
@@ -1,75 +1,59 @@
 package burp;
 
-import java.util.*;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class SimpleImageSizeReader {
 	private static final int MIN_LENGTH = 12;
 
 	public static int[] getImageSize(byte[] image, int offset, int length) {
 		if (length < MIN_LENGTH) return null;
-		int first32be = readInt32be(image, offset);
+		ByteBuffer buf = ByteBuffer.wrap(image, offset, length).order(ByteOrder.BIG_ENDIAN);
+		int first32be = buf.getInt();
 
 		if (first32be == 0x89504e47 && // "\x89PNG"
-				readInt32be(image, offset + 4) == 0x0d0a1a0a) {
-			return getPngSize(image, offset + 8, length - 8);
+				buf.getInt() == 0x0d0a1a0a) {
+			return getPngSize(buf);
 		}
 
 		if (first32be == 0x47494638 && // "GIF8"
-				((image[offset + 4] - (byte)'7') | 2) == 2 && image[offset + 5] == (byte)'a') {
-			return new int[] { readInt16le(image, offset + 6), readInt16le(image, offset + 8) };
+				((buf.get() - (byte)'7') | 2) == 2 && buf.get() == (byte)'a') {
+			buf.order(ByteOrder.LITTLE_ENDIAN);
+			return new int[] { buf.getShort(), buf.getShort() };
 		}
 
 		if (first32be == 0xffd8ffe0 && // JPEG header
-				readInt32be(image, offset + 6) == 0x4a464946 && // "JFIF"
-				image[offset + 10] == 0) {
-			int skip = readInt16be(image, offset + 4) + 4;
-			return getJfifSize(new ByteArrayInputStream(image, offset + skip, length - skip));
+				buf.getInt(6) == 0x4a464946 && // "JFIF"
+				buf.get() == 0) {
+			buf.position(buf.getShort(4) + 4);
+			return getJfifSize(buf);
 		}
 
 		return null;
 	}
 
-	private static int[] getPngSize(byte[] image, int offset, int length) {
-		if (length < 4) return null;
-		int chunkLength = readInt32be(image, offset);
-		if (chunkLength + 12 > length) return null;
-		if (readInt32be(image, offset + 4) == 0x49484452) { // "IHDR"
-			return new int[] { readInt32be(image, offset + 8),
-					readInt32be(image, offset + 12) };
-		} else {
-			return getPngSize(image, offset + chunkLength + 12, length - chunkLength - 12);
-		}
-	}
-
-	private static int[] getJfifSize(final ByteArrayInputStream jfif) {
-		try {
-			byte[] skip = new byte[2];
-			while (jfif.available() > 0) {
-				if (jfif.read() != 0xff) return null;
-				if (jfif.read() == 0xc0) {
-					byte[] payload = new byte[7];
-					if (jfif.read(payload) != 7) return null;
-					return new int[] { readInt16be(payload, 5), readInt16be(payload, 3) };
-				}
-				if (jfif.read(skip) != 2) return null;
-				jfif.skip(readInt16be(skip, 0) - 2);
+	private static int[] getPngSize(ByteBuffer buf) {
+		while (buf.remaining() >= 4) {
+			int chunkLength = buf.getInt();
+			if (chunkLength + 8 > buf.remaining()) return null;
+			if (buf.getInt() == 0x49484452) { // "IHDR"
+				return new int[] { buf.getInt(), buf.getInt() };
 			}
-		} catch (IOException ioe) { /* returning null is OK for this as well */ }
+			buf.position(buf.position() + chunkLength);
+		}
 		return null;
 	}
 
-	private static int readInt32be(byte[] source, int offset) {
-		return (source[offset] << 24) | ((source[offset + 1] & 0xFF) << 16) |
-			((source[offset + 2] & 0xFF) << 8) | (source[offset + 3] & 0xFF);
-	}
-
-	private static int readInt16le(byte[] source, int offset) {
-		return (source[offset + 1] << 8) | source[offset];
-	}
-
-	private static int readInt16be(byte[] source, int offset) {
-		return (source[offset] << 8) | (source[offset + 1] & 0xFF);
+	private static int[] getJfifSize(ByteBuffer buf) {
+		while (buf.hasRemaining()) {
+			if (buf.get() != (byte)0xff) return null;
+			if (buf.get() == (byte)0xc0) {
+				int pos = buf.position();
+				return new int[] { buf.getShort(pos + 5), buf.getShort(pos + 3) };
+			}
+			short skip = buf.getShort();
+			buf.position(buf.position() + skip - 2);
+		}
+		return null;
 	}
 }
